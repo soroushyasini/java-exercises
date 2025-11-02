70513eb35b8b759ece73df68f6f39c2b5d95c55d
Soroush Yasini <soroush.yasini@outlook.com>
2025-11-02 14:23:23 +0330

    doing exercises for page ~20

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+
+    public static void main(String [] args) {
+        int x = 0;
+        int y = 0;
+x = x + 1;
+y = y + x;
+
+
+
+            System.out.print(x + "" + y + " ");
+            x = x + 1;
+        }
+
+    }
+    
+
