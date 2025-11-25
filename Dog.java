f02b921ceabffb5cf266d6530f793b9b549d59f1
Soroush Yasini <soroush.yasini@outlook.com>
2025-11-25 14:52:34 +0330

    start reading after a long time

---
+++ Dog.java
@@ -0,0 +1,15 @@
+class Dog {
+String name;
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.bark();
+        dog1.name = "bart";
+
+        
+
+
+ the code is wrong i suppose.
+
+}
+
+}
