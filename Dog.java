05f48a5a3e1ce4c5e6188c54d8f089e5fc796111
Soroush Yasini <soroush.yasini@outlook.com>
2025-11-25 15:11:46 +0330

    understanding the array refs

--- Dog.java
+++ Dog.java
@@ -1,14 +1,14 @@
 class Dog {
 String name;
     public static void main(String[] args) {
-        Dog dog1 = new Dog();
-        dog1.bark();
-        dog1.name = "bart";
+   //     Dog dog1 = new Dog();
+      //  dog1.bark();
+      //  dog1.name = "bart";
 
         
 
 
- the code is wrong i suppose.
+ //the code is wrong i suppose.
 
 }
 
